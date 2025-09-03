5bd368b329ef7aa3bdbc5d8e846730b926568946
sun <sunny@sunnymorgan.com>
2025-09-03 09:55:17 -0400

    2.0.8

--- aria-labels.php
+++ aria-labels.php
@@ -5,27 +5,29 @@
  *
  * @link              https://github.com/Silver0034/Aria-Labels
  * @since             1.0.0
- * @package           Aria_Labels
+ *
+ * @link              https://github.com/sunmorgn/aria-labels
+ * @since             2.0.4
  *
  * @wordpress-plugin
  * Plugin Name:       Aria Labels
  * Plugin URI:        https://github.com/sunmorgn/aria-labels
  * Description:       Enhances accessibility by adding `aria-hidden` and `aria-label` attributes to Gutenberg blocks. Forked from the original by Jacob Lodes.
- * Version:           2.0.7
+ * Version:           2.0.8
  * Author:            Sunny Morgan
- * Author URI:        
+ * Author URI:
  * Text Domain:       aria-labels
  */
 
 namespace Aria_Labels;
 
 // Stop if this file is called directly.
-if (!defined('WPINC')) die;
+if ( ! defined( 'WPINC' ) ) die;
 
-require_once plugin_dir_path(__FILE__) . 'includes/class-aria-attributes.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-aria-attributes.php';
 new Includes\Aria_Attributes();
 
-if (is_admin()) {
-    require_once plugin_dir_path(__FILE__) . 'includes/class-updater.php';
-    new Includes\Updater();
+if ( is_admin() ) {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-updater.php';
+	new Includes\Updater();
 }
